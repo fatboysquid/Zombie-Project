9f2fa2f400c560eb1d238daef4ddd074e79e18f1
Sang Lee <lasang@gmail.com>
2012-04-15 10:08:29 -0700

    This is the first commit for the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZombieProject::Application.config.secret_token = 'e352cb7d28326a09a79eb923e1b1c56fa0dbae113ab77ac6e697d943619a433ca35c35fef4f496fdffa89eb2f356a5acc332c9ccda9bf35855cf61a53aeb8d42'
