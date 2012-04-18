25cde7572c977e72cee1b7eb942e0c4066dcdc8b
Sang Lee <lasang@gmail.com>
2012-04-17 22:27:08 -0700

    adding reviews resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120417000905) do
+ActiveRecord::Schema.define(:version => 20120418052003) do
+
+  create_table "reviews", :force => true do |t|
+    t.string   "title"
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
